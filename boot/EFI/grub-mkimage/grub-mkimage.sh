43143d3174504e774a0d2144d74238715d7d387c
Hongyi Zhao <hongyi.zhao@gmail.com>
2019-11-01 00:50:29 +0800

    touch up boot/EFI

--- boot/EFI/grub-mkimage/grub-mkimage.sh
+++ boot/EFI/grub-mkimage/grub-mkimage.sh
@@ -1,13 +1,14 @@
 首先准备如下文件：
 grub-mkimage.cfg 
 
-#/dev/sda2                          vfat                       /mnt/sda2                              EF4B-3153
-search.fs_uuid EF4B-3153 root
-# 根据情况修改：
-set prefix=($root)'/EFI/uefi-hd'
-configfile $prefix/grub.cfg
 
-sudo grub-mkimage -c grub-mkimage.cfg -o grub-mkimage-x86_64.efi  -O x86_64-efi -p ''  $( ls -1 /usr/local/lib/grub/x86_64-efi/*.mod | sed 's/\.mod//;s/.*\///' | xargs )
+sudo grub-mkimage -c grub-mkimage.cfg -o grub-mkimage-x86_64.efi -O x86_64-efi -p ''  fat part_msdos part_gpt ext2 search_fs_uuid configfile
 
-加载那些模块就足够了呢？
-sudo grub-mkimage -c grub-mkimage/grub-mkimage.cfg -o grub-mkimage-x86_64.efi  -O x86_64-efi -p '' fat part_msdos part_gpt ext2 regexp search_fs_uuid search configfile
+
+grubconfig.git/boot/EFI$ sudo cp -r * /mnt/sda2/EFI/
+
+sudo efibootmgr --create --disk /dev/sda --part 2 --loader /EFI/grub-mkimage/grub-mkimage-x86_64.efi --label "grub-mkimage-x86_64 hd"
+
+
+这样就可以了，这个就已经不在需要 其他配置文件了。
+
--- boot/EFI/grub-mkstandalone/grub-mkstandalone.sh
+++ boot/EFI/grub-mkstandalone/grub-mkstandalone.sh
@@ -1,11 +1,20 @@
 #接受相对路径，测试如下：
 #$ strings grub-mkstandalone-x86_64.efi | grep -E  '^configfile '
 #configfile ${cmdpath}/grub.cfg
-werner@kubuntu:/mnt/sda2$ sudo grub-mkstandalone -O x86_64-efi -o grub-mkstandalone-x86_64.efi boot/grub/grub.cfg=./grub-mkstandalone/grub-mkstandalone.cfg
+werner@kubuntu:/mnt/sda2$ sudo grub-mkstandalone -O x86_64-efi -o grub-mkstandalone-x86_64.efi boot/grub/grub.cfg=./grub-mkstandalone.cfg
+
+
+grubconfig.git/boot/EFI$ sudo cp -r * /mnt/sda2/EFI/
+
 
 # grub-mkstandalone-x86_64.efi 这个文件必须和它调用的 grub.cfg 在同一个目录：
-sudo efibootmgr --create --disk /dev/sda --part 2 --loader /mnt/sda2/grub-mkstandalone-x86_64.efi --label "grub-mkstandalone-x86_64 hd"
-sudo efibootmgr --create --disk /dev/sda --part 2 --loader /mnt/sda2/grub-mkimage-x86_64.efi --label "grub-mkimage-x86_64 hd"
+
+# --loader 路径写法 ref：
+
+# grubconfig.git/doc/Hybrid-UEFI-GPT+BIOS-GPT-MBR-boot/[steps-used-by-me]grub2-gpt-bios-efi
+
+sudo efibootmgr --create --disk /dev/sda --part 2 --loader /EFI/grub-mkstandalone/grub-mkstandalone-x86_64.efi --label "grub-mkstandalone-x86_64 hd"
+
 
 
 
