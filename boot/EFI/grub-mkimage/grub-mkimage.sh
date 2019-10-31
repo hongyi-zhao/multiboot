eaf2315afac0aa4763593eda4e1d32541bfc6026
Hongyi Zhao <hongyi.zhao@gmail.com>
2019-10-31 18:06:12 +0800

    add EFI config

---
+++ boot/EFI/grub-mkimage/grub-mkimage.sh
@@ -0,0 +1,11 @@
+首先准备如下文件：
+grub-mkimage.cfg 
+
+#/dev/sda2                          vfat                       /mnt/sda2                              EF4B-3153
+search.fs_uuid EF4B-3153 root
+# 根据情况修改：
+set prefix=($root)'/EFI/uefi-hd'
+configfile $prefix/grub.cfg
+
+sudo grub-mkimage -c grub-mkimage.cfg -o grub-mkimage-x86_64.efi  -O x86_64-efi -p ''  $( ls -1 /usr/local/lib/grub/x86_64-efi/*.mod | sed 's/\.mod//;s/.*\///' | xargs )
+
