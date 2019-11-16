2af4125bdad4276712f266c20538fadc2c23c49f
Hongyi Zhao <hongyi.zhao@gmail.com>
2019-11-16 19:39:45 +0800

    touch up oot/efi/EFI/grub-mkstandalone/grub-mkstandalone-pre-load-more-modules

--- boot/efi/EFI/grub-mkstandalone/grub-mkstandalone.sh
+++ boot/efi/EFI/grub-mkstandalone/grub-mkstandalone.sh
@@ -5,6 +5,10 @@
 # --modules='fat part_msdos part_gpt ext2 search_fs_uuid configfile'
 werner@kubuntu:/mnt/sda2$ sudo grub-mkstandalone -O x86_64-efi -o grub-mkstandalone-x86_64.efi --modules='fat part_msdos part_gpt ext2 regexp iso9660 loopback chain search configfile'  boot/grub/grub.cfg=./grub-mkstandalone.cfg
 
+又加入以下预加载的模块：
+grub-mkstandalone -O x86_64-efi -o grub-mkstandalone-x86_64.efi --modules='boot chain configfile ext2 iso9660 loopback memdisk normal ntfs part_gpt part_msdos probe regexp search search_fs_file search_fs_uuid search_label syslinuxcfg tar test tr true' boot/grub/grub.cfg=./grub-mkstandalone.cfg
+
+
 #
 # grub-mkstandalone-x86_64.efi 它调用的 grub.cfg 在同一个目录，就可以了。
 
