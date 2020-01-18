2dde376abff7bf5b2f0abbd932a090f4b56e7391
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-01-18 19:10:43 +0800

    touchup modified:   EFI/grub-mkstandalone/grub-mkstandalone.sh modified:   boot/grub/custom.cfg

--- EFI/grub-mkstandalone/grub-mkstandalone.sh
+++ EFI/grub-mkstandalone/grub-mkstandalone.sh
@@ -5,9 +5,10 @@
 # --modules='fat part_msdos part_gpt ext2 search_fs_uuid configfile'
 
 
-又加入以下预加载的模块：
-这些模块够用就行，后续需要再加载即可。
-另外，当使用grub-mkstandalone的时候，还会自动预加载 memdisk 、tar 两个模块。
+#又加入以下预加载的模块：
+#这些模块够用就行，后续需要再加载即可。
+#另外，当使用grub-mkstandalone的时候，还会自动预加载 memdisk 、tar 两个模块。
+
 grub-mkstandalone -O x86_64-efi -o grubx64.efi --modules='fat part_msdos part_gpt ext2 probe regexp search configfile' boot/grub/grub.cfg=./boot-grub-grub.cfg
 
 
@@ -18,20 +19,23 @@ grub-mkstandalone -O x86_64-efi -o grubx64.efi --modules='fat part_msdos part_gp
 
 # grubconfig.git/doc/Hybrid-UEFI-GPT+BIOS-GPT-MBR-boot/[steps-used-by-me]grub2-gpt-bios-efi
 
-参考后来gmail，arch maillist的讨论，似乎应该用下面的形式：
+# 无论采用何种写法，最后以 efibootmgr 查看的结果为准：必须能够被efibootmgr
+# 正确识别相应的路径。否则，就是错误的写法。
 
---loader \\EFI\\grub-mkstandalone\\grub-mkstandalone-x86_64.efi
+#参考后来gmail，arch maillist的讨论，似乎应该用下面的形式：
+
+#--loader \\EFI\\grub-mkstandalone\\grub-mkstandalone-x86_64.efi
 
 
-sudo efibootmgr --create --disk /dev/sda --part 1 --loader \\EFI\\grub-mkstandalone\\grubx64.efi -w --label "grub-mkstandalone-grubx64 hd"
+#sudo efibootmgr --create --disk /dev/sda --part 1 --loader \\EFI\\grub-mkstandalone\\grubx64.efi -w --label "grub-mkstandalone-grubx64 hd"
 
-sudo efibootmgr --create --disk /dev/sdc --part 1 --loader \\EFI\\grub-mkstandalone\\grubx64.efi -w --label "grub-mkstandalone-grubx64 usb p1"
+#sudo efibootmgr --create --disk /dev/sdc --part 1 --loader \\EFI\\grub-mkstandalone\\grubx64.efi -w --label "grub-mkstandalone-grubx64 usb p1"
 
-sudo efibootmgr --create --disk /dev/sdc --part 2 --loader \\EFI\\grub-mkstandalone\\grubx64.efi -w --label "grub-mkstandalone-grubx64 usb p2"
+#sudo efibootmgr --create --disk /dev/sdc --part 2 --loader \\EFI\\grub-mkstandalone\\grubx64.efi -w --label "grub-mkstandalone-grubx64 usb p2"
 
-实际测试，下面的形式也可以：
+#实际测试，下面的形式也可以：
 
-sudo efibootmgr -c -d /dev/sda -p 1 -w -L 'grub-mkstandalone-grubx64 hd' -l /EFI/grub-mkstandalone/grubx64.efi
+#sudo efibootmgr -c -d /dev/sda -p 1 -w -L 'grub-mkstandalone-grubx64 hd' -l /EFI/grub-mkstandalone/grubx64.efi
 
 
 
